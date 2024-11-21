fa454fc45bb505306d5cf02f8119707c041d1d32
AfonsoAvila <afonso.avila2001@gmail.com>
2024-11-21 23:14:23 +0000

    Update settings.py

--- monitor_trafego/settings.py
+++ monitor_trafego/settings.py
@@ -29,9 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yr0qbb9f11i608dvu-rx+b^_=4f6ex-!-z1ds$1%$qwnogta7%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
